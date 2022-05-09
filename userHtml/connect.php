b2d485e270c93356d0b29cf1e28b6c6bbd05799b
Implax <juniourclottey@gmail.com>
2022-05-09 16:16:18 +0000

    everything

--- userHtml/connect.php
+++ userHtml/connect.php
@@ -1,8 +1,8 @@
 <?php
-$svrname = "localhost";
-$uname = "root";
-$pswd = "";
-$db ="weconnect";
+$svrname = "us-cdbr-east-05.cleardb.net";
+$uname = "ba0489cfbd42c2";
+$pswd = "4b667e92";
+$db ="heroku_7c5b6c3bdf49a1f";
 
 // Create connection
 $conn = new mysqli($svrname, $uname, $pswd,$db);
