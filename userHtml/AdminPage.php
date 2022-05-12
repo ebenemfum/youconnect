dda41a9de4473cd2dda6bb8a8c015bfaea5be7ae
ebenemfum <65869086+ebenemfum@users.noreply.github.com>
2022-05-12 03:28:19 +0000

    Uploaded

--- userHtml/AdminPage.php
+++ userHtml/AdminPage.php
@@ -1,6 +1,5 @@
 <?php
-    include "connect.php";
-    
+    include "connect.php";   
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,14 +8,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin User</title>
-    <link rel="stylesheet" href="AdminPage.css">
+    <link rel="stylesheet" href="../ADMIN/AdminPage.css">
     <script src="https://kit.fontawesome.com/2ac6a790f8.js" crossorigin="anonymous"></script>
-<!--     <script src="https://kit.fontawesome.com/2ac6a790f8.js" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/2ac6a790f8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> ≠≠
 
 </head>
 <body>
@@ -41,6 +40,19 @@
         <a href="SoftwareDevUser.html"><i class="fas fa-bars-progress"></i> Sofware Dev Users<span></span> </a>
         
     </div>
+
+    <br>
+    <br>
+
+    <br>
+    <br>
+
+    <br>
+    <br>
+
+    <br>
+    <br>
+
     <div class="container">
       <div class="jumbotron">
              <div class="card_main">
